ea630258b487d624d2f087d1376873dd97878c53
ssss0578 <sm01243@yahoo.co.jp>
2025-09-24 11:51:17 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,22 @@
+package code11_ex01;
+
+public abstract class  TangibleAsset {
+	String name;
+	int price;
+	String color;
+	
+	public TangibleAsset(String name, int price, String color) {
+		this.name = name;                //初期化　コンストラクタ
+		this price = praice;             //初期化　コンストラクタ
+		this.color = color;              //初期化　コンストラクタ
+	}
+	  
+	public String getName() {reture this.name; }
+	
+	public int getPrice() {reture this.price; }
+	
+	public String getColor() {reture this.color; }
+	
+	public String getName() {reture this.name; }
+
+}
